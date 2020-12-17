ab5d769fc5997fe630ca0fcc1145ba2ad96e89d6
KDreamsss <73767332+KDreamsss@users.noreply.github.com>
2020-12-17 23:20:08 +0700

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,14 +5,14 @@
     <div class="row justify-content-center" style="margin:20px">
         <div class="col-md-8" style="padding: 15px;">
             <div class="card">
-                <div class="card-header"><h1>Register to be Our Mamily Member</h1></div>
+                <div class="card-header"><h1>{[ __('Register to be Our Family Member') }}</h1></div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">Username</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __'Username') }}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
