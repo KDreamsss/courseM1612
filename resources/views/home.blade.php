b260ca1bead91fbeaa4232337a007e3f77f7cbc3
KDreamsss <73767332+KDreamsss@users.noreply.github.com>
2020-12-17 15:43:36 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,11 +22,10 @@
                                 var i = document.getElementById('counter');
                                 if (parseInt(i.innerHTML)<=0) {
                                     location.href = 'students';
-                                    return false;
                                 }
                                 if (parseInt(i.innerHTML)!=0) {
                                     i.innerHTML = parseInt(i.innerHTML)-1;
-                                }
+                                } return false;
                             }
                             setInterval(function(){ countdown(); },1000);
                         </script>
