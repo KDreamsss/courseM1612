57bd1c7940a9d2329b300be1afc2c249b230fe61
KDreamsss <73767332+KDreamsss@users.noreply.github.com>
2020-12-17 23:14:01 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,8 +44,8 @@
         }
         body {
             background-image:  url("https://www.thairath.co.th/media/dFQROr7oWzulq5FZYAIlMVEofzVWimHZE3b1GKBHvKmOsMTwQlFz4ebiQGZLWv4u27b.jpg");
-	    background-repeat: no-repeat;
-	    background-size: cover;
+	        background-repeat: no-repeat;
+	        background-size: cover;
         }
 
     </style>
@@ -56,7 +56,7 @@
         <nav class="header navbar navbar-expand-md navbar-dark shadow-sm ">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/login') }}">
-                    <h1>Course Management</h1>
+                    <h1>{{ __('Course Management' }}</h1>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -73,13 +73,13 @@
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <h2><a class="nav-link" href="{{ route('login') }}">Login</a></h2>
+                                    <h2><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></h2>
                                 </li>
                             @endif
                             
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <h2><a class="nav-link" href="{{ route('register') }}">Register</a></h2>
+                                    <h2><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></h2>
                                 </li>
                             @endif
                         @else
@@ -92,7 +92,7 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        Logout
+                                        {{ __('Logout') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
